bbf1fedd63f45d8fbcd62e4c09c0c6d583383f7f
bolid <bratik.n@yandex.ru>
2022-01-10 11:45:50 +0300

    java 30.11.2021

--- 8.Games/src/com/javarush/games/spaceinvaders/ShapeMatrix.java
+++
@@ -1,9 +0,0 @@
-package com.javarush.games.spaceinvaders;
-
-public class ShapeMatrix {
-    public static final int[][] ENEMY = new int[][]{
-            {7, 0, 7},
-            {7, 7, 7},
-            {0, 7, 0}
-    };
-}
--- 8.Games/src/com/javarush/games/spaceinvaders/gameobjects/Star.java
+++ 8.Games/src/com/javarush/games/spaceinvaders/gameobjects/Star.java
@@ -1,22 +1,6 @@
 package com.javarush.games.spaceinvaders.gameobjects;
 
-import com.javarush.engine.cell.Color;
-import com.javarush.engine.cell.*;
+import com.javarush.engine.cell.Game;
 
-public class Star extends GameObject{
-
-    public Star(double x, double y) {
-        super(x, y);
-    }
-
-    private static final String STAR_SIGN = "\u2605";
-
-
-     public void draw(Game game){
-
-        game.setCellValueEx((int)x, (int)y, Color.NONE,STAR_SIGN,Color.WHITE,100);
-
-    }
-
-
+public class Star extends Game {
 }
